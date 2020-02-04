b7afb9f8d38e7f37c569c140fceff34533a96a38
yijiahuang <hyj@iastate.edu>
2020-02-04 15:39:01 -0600

    update left right side with one loc

--- src-test/boa_2020/test.java
+++ src-test/boa_2020/test.java
@@ -2,19 +2,10 @@ package boa_2020;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevSort;
-import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.refactoringminer.api.GitHistoryRefactoringMiner;
 import org.refactoringminer.api.GitService;
@@ -22,12 +13,6 @@ import org.refactoringminer.api.Refactoring;
 import org.refactoringminer.api.RefactoringHandler;
 import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;
 import org.refactoringminer.util.GitServiceImpl;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 public class test {
 	
@@ -39,7 +24,7 @@ public class test {
 		File gitDir = new File("/Users/hyj/git/BoaData/GitBareRepos/refactoring-toy-example.git");
 		try {
 			Repository repo = new FileRepositoryBuilder().setGitDir(gitDir).build();
-			miner.detectAtCommit(repo, "05c1e773878bbacae64112f70964f4f2f7944398", new RefactoringHandler() {
+			miner.detectAtCommit(repo, "0e193b7d02902c6f2abf7c88eebe937d1ac5fc51", new RefactoringHandler() {
 				@Override
 				public void handle(String commitId, List<Refactoring> refactorings) {
 					System.out.println("Refactorings at " + commitId);
@@ -57,5 +42,6 @@ public class test {
 			e.printStackTrace();
 		}
 	}
+
 
 }
--- src/org/refactoringminer/api/Refactoring.java
+++ src/org/refactoringminer/api/Refactoring.java
@@ -1,7 +1,10 @@
 package org.refactoringminer.api;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+
+import gr.uom.java.xmi.diff.CodeRange;
 
 public interface Refactoring extends Serializable, CodeRangeProvider {
 
@@ -20,9 +23,15 @@ public interface Refactoring extends Serializable, CodeRangeProvider {
 		sb.append("{").append("\n");
 		sb.append("\t").append("\"").append("type").append("\"").append(": ").append("\"").append(getName()).append("\"").append(",").append("\n");
 		sb.append("\t").append("\"").append("description").append("\"").append(": ").append("\"").append(toString().replace('\t', ' ')).append("\"").append(",").append("\n");
-		sb.append("\t").append("\"").append("leftSideLocations").append("\"").append(": ").append(leftSide()).append(",").append("\n");
-		sb.append("\t").append("\"").append("rightSideLocations").append("\"").append(": ").append(rightSide()).append("\n");
+		sb.append("\t").append("\"").append("leftSideLocations").append("\"").append(": ").append(getFirst(leftSide())).append(",").append("\n");
+		sb.append("\t").append("\"").append("rightSideLocations").append("\"").append(": ").append(getFirst(rightSide())).append("\n");
 		sb.append("}");
 		return sb.toString();
+	}
+	
+	default List<CodeRange> getFirst(List<CodeRange> list) {
+		List<CodeRange> l = new ArrayList<CodeRange>();
+		l.add(list.get(0));
+		return l;
 	}
 }
